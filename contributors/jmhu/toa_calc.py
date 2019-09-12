b39889457233c65343547be3af2baaeab7deb5c1
J. Michelle Hu <jmhu@uw.edu>
2019-09-12 06:29:43 +0000

    Remove excess code, comment print statements

--- contributors/jmhu/toa_calc.py
+++ contributors/jmhu/toa_calc.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
+'''
+Adapted from https://developers.planet.com/tutorials/convert-planetscope-imagery-from-radiance-to-reflectance/
+
+Usage: python ~/git_dirs/ghw2019_planetpieces/scripts/toa_calc.py path_to_Analytic.tif
+
+'''
 # import libraries
 import argparse
 import numpy as np
@@ -10,7 +16,7 @@ start_time = time.time()
 
 # Have user define input bands and output filename
 parser = argparse.ArgumentParser(description='GeoTiff Planet Multispectral Image Radiance to TOA Reflectance Conversion Script')
-parser.add_argument('-in', '--input_file', help='Planet Analytic.tif MS image file', required=True)
+parser.add_argument('input_file', help='Planet *Analytic.tif MS image file')
 
 args = parser.parse_args()
 
@@ -22,71 +28,76 @@ print("Filename is: ", filename)
 print("XML is: ", xml_fn)
 print("Out filename is: ", out_fn)
 
-# Load all bands as numpy arrays - note all PlanetScope 4-band images have band order BGRN
-with rasterio.open(filename) as src:
-# Alternative method, seems to take the same amount of time
-#     f=src.read()
-#     band_blue_radiance = f[0, :, :]
-#     band_green_radiance = f[1, :, :]
-#     band_red_radiance = f[2, :, :]
-#     band_nir_radiance = f[3, :, :]
-    
-    band_blue_radiance = src.read(1)
-    band_green_radiance = src.read(2)
-    band_red_radiance = src.read(3)
-    band_nir_radiance = src.read(4)
-    
-# print('Shapes are: ', band_blue_radiance.shape, '\n',
-#      band_green_radiance.shape, '\n',
-#      band_red_radiance.shape, '\n',
-#      band_nir_radiance.shape, '\n',)
-    
-xmldoc = minidom.parse(xml_fn)
-nodes = xmldoc.getElementsByTagName("ps:bandSpecificMetadata")
+try:
+    with rasterio.open(out_fn) as src:
+        print("Reflectance file exists")
 
-# XML parser refers to bands by numbers 1-4
-coeffs = {}
-for node in nodes:
-    bn = node.getElementsByTagName("ps:bandNumber")[0].firstChild.data
-    if bn in ['1', '2', '3', '4']:
-        i = int(bn)
-        value = node.getElementsByTagName("ps:reflectanceCoefficient")[0].firstChild.data
-        coeffs[i] = float(value)
+except:
+    # Load all bands as numpy arrays - note all PlanetScope 4-band images have band order BGRN
+    with rasterio.open(filename) as src:
+    # Alternative method, seems to take the same amount of time
+    #     f=src.read()
+    #     band_blue_radiance = f[0, :, :]
+    #     band_green_radiance = f[1, :, :]
+    #     band_red_radiance = f[2, :, :]
+    #     band_nir_radiance = f[3, :, :]
 
-print("Conversion coefficients:", coeffs)
+        band_blue_radiance = src.read(1)
+        band_green_radiance = src.read(2)
+        band_red_radiance = src.read(3)
+        band_nir_radiance = src.read(4)
 
+    # print('Shapes are: ', band_blue_radiance.shape, '\n',
+    #      band_green_radiance.shape, '\n',
+    #      band_red_radiance.shape, '\n',
+    #      band_nir_radiance.shape, '\n',)
 
-# Multiply the Digital Number (DN) values in each band by the TOA reflectance coefficients
-band_blue_reflectance = band_blue_radiance * coeffs[1]
-band_green_reflectance = band_green_radiance * coeffs[2]
-band_red_reflectance = band_red_radiance * coeffs[3]
-band_nir_reflectance = band_nir_radiance * coeffs[4]
+    xmldoc = minidom.parse(xml_fn)
+    nodes = xmldoc.getElementsByTagName("ps:bandSpecificMetadata")
 
-# print("Red band radiance is from", np.amin(band_red_radiance), "to", np.amax(band_red_radiance))
-# print("Red band reflectance is from", np.amin(band_red_reflectance), "to", np.amax(band_red_reflectance))
+    # XML parser refers to bands by numbers 1-4
+    coeffs = {}
+    for node in nodes:
+        bn = node.getElementsByTagName("ps:bandNumber")[0].firstChild.data
+        if bn in ['1', '2', '3', '4']:
+            i = int(bn)
+            value = node.getElementsByTagName("ps:reflectanceCoefficient")[0].firstChild.data
+            coeffs[i] = float(value)
 
-# Set spatial characteristics of the output object to mirror the input
-kwargs = src.meta
-kwargs.update(
-    dtype=rasterio.uint16,
-    count = 4)
-# print("Before SCALING, red band reflectance is from", np.amin(band_red_reflectance), "to", np.amax(band_red_reflectance))
+    print("Conversion coefficients:", coeffs)
 
 
-# Here we include a fixed scaling factor. This is common practice.
-scale = 10000
-blue_ref_scaled = scale * band_blue_reflectance
-green_ref_scaled = scale * band_green_reflectance
-red_ref_scaled = scale * band_red_reflectance
-nir_ref_scaled = scale * band_nir_reflectance
-# print("After SCALING, red band reflectance is from", np.amin(red_ref_scaled), "to", np.amax(red_ref_scaled))
+    # Multiply the Digital Number (DN) values in each band by the TOA reflectance coefficients
+    band_blue_reflectance = band_blue_radiance * coeffs[1]
+    band_green_reflectance = band_green_radiance * coeffs[2]
+    band_red_reflectance = band_red_radiance * coeffs[3]
+    band_nir_reflectance = band_nir_radiance * coeffs[4]
 
-# Write band calculations to a new raster file
-with rasterio.open(out_fn, 'w', **kwargs) as dst:
-        dst.write_band(1, blue_ref_scaled.astype(rasterio.uint16))
-        dst.write_band(2, green_ref_scaled.astype(rasterio.uint16))
-        dst.write_band(3, red_ref_scaled.astype(rasterio.uint16))
-        dst.write_band(4, nir_ref_scaled.astype(rasterio.uint16))
+    # print("Red band radiance is from", np.amin(band_red_radiance), "to", np.amax(band_red_radiance))
+    # print("Red band reflectance is from", np.amin(band_red_reflectance), "to", np.amax(band_red_reflectance))
+
+    # Set spatial characteristics of the output object to mirror the input
+    kwargs = src.meta
+    kwargs.update(
+        dtype=rasterio.uint16,
+        count = 4)
+    # print("Before SCALING, red band reflectance is from", np.amin(band_red_reflectance), "to", np.amax(band_red_reflectance))
+
+
+    # Here we include a fixed scaling factor. This is common practice.
+    scale = 10000
+    blue_ref_scaled = scale * band_blue_reflectance
+    green_ref_scaled = scale * band_green_reflectance
+    red_ref_scaled = scale * band_red_reflectance
+    nir_ref_scaled = scale * band_nir_reflectance
+    # print("After SCALING, red band reflectance is from", np.amin(red_ref_scaled), "to", np.amax(red_ref_scaled))
+
+    # Write band calculations to a new raster file
+    with rasterio.open(out_fn, 'w', **kwargs) as dst:
+            dst.write_band(1, blue_ref_scaled.astype(rasterio.uint16))
+            dst.write_band(2, green_ref_scaled.astype(rasterio.uint16))
+            dst.write_band(3, red_ref_scaled.astype(rasterio.uint16))
+            dst.write_band(4, nir_ref_scaled.astype(rasterio.uint16))
         
 elapsed_time = time.time() - start_time
 print("Total time elapsed for toa_calc.py is:", "{0:.4}".format(elapsed_time), "seconds")
